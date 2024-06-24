a0094f459c93a3154be0d30fdc57bfcbc26be3a6
oleksandr_zamuruiev <zamuruev@i.ua>
2024-06-24 15:50:38 +0300

    Добавление обработки ошибок в функцию fetchImages

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -14,19 +14,23 @@ export const fetchImages = async (query, page = 1) => {
     currentPage = page;
   }
 
-  const response = await axios.get(BASE_URL, {
-    params: {
-      key: API_KEY,
-      q: query,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: currentPage,
-      per_page: 15,
-    },
-  });
-
-  return response.data;
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: currentPage,
+        per_page: 15,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching images from Pixabay:', error);
+    throw error;
+  }
 };
 
 export const getNextPage = () => currentPage + 1;
